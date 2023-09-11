b09fde3ac3ee9944bab8fe553e556d0b9f177559
Suyama Daiki <Ditel@gmail.com>
2023-09-11 20:27:01 +0900

    v1.0.1

---
+++ Slave_src/Ditel_ROS_Slave.hpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#define ADDRESS 1
+
+#define MEMORY_SIZE_SERIAL_TASK 128
+#define MEMORY_SIZE_USER_PROGRAM_TASK 128
+
+class Ditel_Serial
+{
+private:
+
+public:
+    void begin()
+    {
+    }
+};
---
+++ Slave_src/User_main.hpp
@@ -0,0 +1,10 @@
+#include "Ditel_ROS_Slave.hpp"
+
+void _setup(){
+    
+}
+
+void _loop(){
+
+}
+
